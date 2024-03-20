5212b65c017d79bdc001293f67a509fead8d51e7
WilliWasHere <164195210+WilliWasHere@users.noreply.github.com>
2024-03-20 22:09:43 +0700

    Add files via upload

---
+++ halaman_baru.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ucen Bro</title>
+</head>
+<body>
+    <h1>Ucen Bro</h1>
+
+    <p>selamat datang dirumah ucen broo.</p>
+
+    <!-- Tombol yang mengarahkan ke halaman baru -->
+    <a href="index.php">home</a>
+</body>
+</html>
